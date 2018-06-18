b3ae5c7b39a7ba51510c61ba57ff2df02853bb0c
TakumiKanda <a5816027@aoyama.jp>
2018-06-18 17:18:48 +0900

    second commit

---
+++ pythontest/p9_3.py
@@ -0,0 +1,15 @@
+import matplotlib.pyplot as plt
+
+steps = [148376, 149539, 150699, 152262, 153901, 155779, 157450]
+labels = ['2012', '2013', '2014', '2015', '2016', '2017', '2018']
+num_bars = len(steps)
+positions = range(1, num_bars+1)
+plt.barh(positions, steps, align='center')
+
+plt.yticks(positions, labels)
+plt.xlabel('Population')
+plt.ylabel('Year')
+plt.title('Population transition of Tokorozawa city')
+ 
+plt.grid()
+plt.show()
